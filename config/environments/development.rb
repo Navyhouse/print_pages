ae7913df0d61b5117ab3d1a9063262f92c4638b7
Navyhouse <kjo0902.biz@gmail.com>
2020-12-23 11:58:11 +0900

    mailtest

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # default url
-  config.action_mailer.default_url_options =　{ :host => 'http://printpages.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000', port: 3000 }
   # mail setting
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,9 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { :host => 'http://アプリ名.herokuapp.com' }
-  
-  config.action_mailer.default_url_options = { host: 'kjo0902.bis@gmail.com'}
+  config.action_mailer.default_url_options = { :host => 'http://printpages.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
